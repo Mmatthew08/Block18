5e5258608448c6f5f303131b54edfef00160c346
Matthew Eski <metehaneski08@gmail.com>
2023-10-24 13:40:09 -0500

    notes

---
+++ guidedPractice.md
@@ -0,0 +1,6 @@
+Happy:
+pushing down botton should indicate for lower floors. 
+pushing up button should indicate for upper floors.
+pushing open doors button should open doors
+Unhappy:
+Pushing open doors button when the doors already open should not do anything.
---
+++ notes.js
@@ -0,0 +1,8 @@
+//Testing:
+// Easier debugging,increased confidence in changes,better collabration,improved code quality.
+// TDD(Test-Driven Development) is an software development process that follows a simple INTERATIVE CYCLE
+// Failure is Expected.
+
+//Types of Testing: Unit: Checks the functionality of an unit of code. Integration: Testing combinining individial modules and testing if they work together.
+//Functional: Testing user sceneiros on the browser itself by controlling the browser programittcally.Acceptance: Testing that the application meets the requirements
+// Steps of Writing Unit Tests: 1- Read the Problem 2- Identify expectations 3- Write pseudocode 4- Translate pseudocode to real code.
